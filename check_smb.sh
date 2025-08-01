4a13deb472d68c0e7c46bacf57208419b48a5d57
kali <kali@kali>
2025-08-01 13:29:18 -0700

    fixed awk command

--- check_smb.sh
+++ check_smb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nmap -p 139, 445 --open -iL windows.txt -oG - | awk '/(139\445)\/open/ {print $2}' | sort -u > smb.txt
+nmap -p 139,445 --open -iL "$1" -oG - | awk '/139\/open/ || /445\/open/ {print $2}' | sort -u > smb.txt
 
-nmap -p 139, 445 --script smb-vuln* -iL smb.txt
+nmap -p 139,445 --script smb-vuln-* -iL smb.txt
